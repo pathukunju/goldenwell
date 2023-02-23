237194c1bc103da74703583d4feeb69b6c15fa7c
pathukunju <pathukunju1993@gmail.com>
2023-02-23 16:47:42 +0530

    first commit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('dashboard-index/',views.dashboard_index, name='index_dashboard'),
     path('admin_dashboard/',views.admin_dashboard, name='admin_dashboard'),
     path('signout/',views.signout, name='logout'),
-    path('gw-admin/',views.LoginView.as_view(),name='gw-admin')
+    path('',views.LoginView.as_view(),name='gw-admin')
    
     
 ]
